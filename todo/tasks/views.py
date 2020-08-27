ed9ca2c8251cbadde14727b734233fe7e0a5b5ee
SevenNateNine <nate.chan29@gmail.com>
2020-08-27 09:22:01 -0400

    Added sidebar and adjusted navbar, installed Django FontAwesome for icons as well

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -10,3 +10,19 @@ def index(request):
     latest_tasks = Task.objects.order_by('-updated_date')
     context = {'latest_task_list' : latest_tasks}
     return render(request, 'todo/index.html', context)
+
+# show specific task
+def detail(request, task_id):
+    pass
+
+# delete specific task
+def delete(request, task_id):
+    pass
+
+# mark task as complete
+def complete(request, task_id):
+    pass
+
+# add new task, probably will need more parameters
+def add_task(request):
+    pass
--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'tasks.apps.TaskConfig',
+    "django_static_fontawesome",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
