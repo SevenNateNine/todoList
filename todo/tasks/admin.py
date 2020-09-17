17b58141263ea0a417f59bfd1eea742f0a9335da
SevenNateNine <nate.chan29@gmail.com>
2020-09-17 12:22:50 -0400

    Added Plan model to help consolidate tasks.

--- todo/tasks/admin.py
+++ todo/tasks/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Task
+from .models import Task, Plan
 
 admin.site.site_header = "Tasks Admin"
 admin.site.site_title = "Tasks Admin Area"
@@ -12,14 +12,30 @@ class TaskAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['task_title']}), 
         (None, {'fields': ['task_description']}), 
-        (None, {'fields': ['completed']}), 
+        (None, {'fields': ['completed', 'important', 'archive']}), 
         (None, {'fields': ['author']}), 
+        (None, {'fields': ['parent_plan']}), 
         ('Date Information', 
             {
-                'fields': ['creation_date', 'updated_date'], 
+                'fields': ['creation_date', 'updated_date', 'due_date'], 
                 'classes':['collapse'],
             }
         )    
     ]
 
+class PlanAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['plan_title']}), 
+        (None, {'fields': ['plan_description']}), 
+        (None, {'fields': ['completed', 'important', 'archive']}), 
+        (None, {'fields': ['author']}), 
+        ('Date Information', 
+            {
+                'fields': ['creation_date', 'updated_date', 'due_date'], 
+                'classes':['collapse'],
+            }
+        )    
+    ]
+
+admin.site.register(Plan, PlanAdmin)
 admin.site.register(Task, TaskAdmin)
--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -2,18 +2,44 @@ from django.db import models
 from django.conf import settings
 import datetime
 
+class Plan(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    plan_title = models.CharField(max_length=200)
+    plan_description = models.CharField(max_length=200, blank=True, null=True)
+    creation_date = models.DateTimeField('date created', default=datetime.datetime.now())
+    updated_date = models.DateTimeField('date updated', default=datetime.datetime.now())
+    due_date = models.DateTimeField('date updated', blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    important = models.BooleanField(default=False)
+    archive = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.plan_title
+
 class Task(models.Model):
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
     )
+    parent_plan = models.ForeignKey(
+        Plan,
+        on_delete=models.CASCADE,
+        null = True,
+    )
     task_title = models.CharField(max_length=200)
     task_description = models.CharField(max_length=200, blank=True, null=True)
     creation_date = models.DateTimeField('date created', default=datetime.datetime.now())
     updated_date = models.DateTimeField('date updated', default=datetime.datetime.now())
+    due_date = models.DateTimeField('date updated', blank=True, null=True)
     completed = models.BooleanField(default=False)
+    important = models.BooleanField(default=False)
+    archive = models.BooleanField(default=False)
 
     def __str__(self):
         return self.task_title
+
 
 
